8abfb44b7ce7fb8eb561a22f4e9e651596e9699f
Nick Stenquist <whiteshadow148@gmail.com>
2020-09-04 16:30:09 -0600

    Fixed on attribute error cause another

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedMananger(models.Manager):
+    def get_queryset(self):
+        return super(PublishedMananger, self).get_queryset().filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -9,17 +13,21 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,
-                            unique_for_date='published')
-    author = models.ForeignKey(User, 
-                                on_delete=models.CASCADE,
-                                related_name='blog_posts')
+                            unique_for_date='publish')
+    author = models.ForeignKey(User,
+                              on_delete=models.CASCADE,
+                              related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
-                                choices=STATUS_CHOICES,
-                                default='draft')
+                              choices=STATUS_CHOICES,
+                              default='draft')
+
+    objects = models.Manager() # The default manager
+    published = PublishedMananger() # Our custom manager
+    
     class Meta:
         ordering = ('-publish',)
     def __str__(self):
