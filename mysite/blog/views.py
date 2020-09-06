b83fdb79578a0186e90aed28fe09a7077f2fdcb7
Nick Stenquist <whiteshadow148@gmail.com>
2020-09-05 20:14:22 -0600

    Created and tested share page and emails sent

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -33,13 +33,23 @@ class PostListView(ListView):
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
     if request.method == 'POST':
-        #Form was submitted
+        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            #Form fields passed validation
-            cd = form.cleaned_data()
-            # ... send email
-        else:
-            form = EmailPostForm()
-        return render(request, 'blog/post/share.html', {'post':post, 'form':form})
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
