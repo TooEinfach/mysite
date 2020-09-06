a9ee13f6ce7ed46615e08766a21fffa07cddba16
Nick Stenquist <whiteshadow148@gmail.com>
2020-09-05 20:54:14 -0600

    Adding ability to make comments to a blog post

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
 from django.urls import reverse
+from django.contrib.auth.models import User
+# from taggit.managers import TaggableManager
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -29,6 +31,13 @@ class Post(models.Model):
 
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
+    # tags = TaggableManager()
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
@@ -36,8 +45,20 @@ class Post(models.Model):
                              self.publish.month,
                              self.publish.day, self.slug])
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
     class Meta:
-        ordering = ('-publish',)
+        ordering = ('created',)
 
     def __str__(self):
-        return self.title
+        return f'Comment by {self.name} on {self.post}'
